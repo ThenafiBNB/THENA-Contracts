83c003fc2f9c38b364ebf4b98cf9cfc9b99380d5
minhtrih <hmt18101996@gmail.com>
2023-02-14 16:48:57 -0800

    update routerV2

---
+++ scripts/deploy/RouterV2.js
@@ -0,0 +1,19 @@
+// // scripts/upgrade-box.js
+const { ethers, upgrades } = require("hardhat");
+const { deployContract, contractAt, writeTmpAddresses } = require("../shared/helpers");
+require("dotenv").config();
+
+async function main() {
+  await deployContract("RouterV2",
+    [
+      process.env.PAIRFACTORYUPGRADEABLE,
+      process.env.WFTM
+    ], "deploy RouterV2");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
