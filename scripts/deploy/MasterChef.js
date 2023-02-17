ca3d2e22968e22345a6ae41d64e0ca4b55e5f55a
huyminh1115 <huyminh1115@gmail.com>
2023-02-18 00:36:02 +0700

    Add fuction in script MasterChef

--- scripts/deploy/MasterChef.js
+++ scripts/deploy/MasterChef.js
@@ -13,6 +13,8 @@ async function main() {
   let contract = await contractAt("MasterChef", process.env.MASTERCHEF);
 
   await sendTxn(contract.addKeeper([process.env.PUBLICKEY]), "MasterChef.addKeeper");
+  // QUAN TRỌNG CẦN staking fee convert
+  await sendTxn(contract.addKeeper([process.env.STAKINGCONVERTER]), "MasterChef.addKeeper");
   await sendTxn(contract.setDistributionRate(ethers.utils.parseUnits("10", 18)), "MasterChef.setDistributionRate");
   // console.log(await contract.isKeeper(process.env.PUBLICKEY));
   // more
